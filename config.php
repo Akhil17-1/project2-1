6a0b874f3466c436399642d477cce0a650ddf84f
akhilkothakota <36935657+akhilkothakota@users.noreply.github.com>
2023-04-12 17:21:59 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<!-- db config -->
+<?php
+// Path: config.php
+// DB credentials.
+define('DB_HOST','localhost');
+define('DB_USER','akhil');
+define('DB_PASS','akhil123');
+define('DB_NAME','project2');
+// Establish database connection.
+$db = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  
+?>
